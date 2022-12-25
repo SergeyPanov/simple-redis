e28edcf2896b885bde21e11ebcd39e961db305bd
Sergey Panov <panovsy@gmail.com>
2022-12-25 13:32:36 +0100

    reply to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{Write},
-    net::TcpListener,
+    io::{Write, BufReader, BufRead},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -15,13 +15,49 @@ fn main() {
     for stream in listener.incoming() {
         let response = "+PONG\r\n";
 
-        match stream.unwrap().write_all(response.as_bytes()) {
-            Ok(_) => {
-                println!("Replied with {}", response);
+        match stream {
+            Ok(mut _stream) => {
+                loop {
+                    read_resp_request(&_stream);
+                    if let Err(e) = _stream.write_all(response.as_bytes()) {
+                        println!("err: {}", e);
+                        break;
+                    }
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
+    }
+}
+
+fn read_resp_request(stream: &TcpStream) {
+    let mut reader = BufReader::new(stream);
+    let len = resp_array_len(&mut reader);
+
+    for _ in 0..len {
+        let mut buf = String::new();
+
+        match reader.read_line(&mut buf) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("Error: {}", e)
+            },
+        }
+    }
+}
+
+fn resp_array_len(reader: &mut BufReader<&TcpStream>) -> i32 {
+    let mut buf = String::new();
+
+    match reader.read_line(&mut buf) {
+        Ok(_) => buf
+                    .chars()
+                    .filter(|c| c.is_ascii_digit())
+                    .collect::<String>()
+                    .parse::<i32>()
+                    .unwrap_or_default(),
+        Err(_) => 0,
     }
 }
